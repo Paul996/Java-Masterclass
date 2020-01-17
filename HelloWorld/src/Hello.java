8860bd037ff43b50befc75c1c47ad43c06e231ec
Paul886 <pliu2468@gmail.com>
2020-01-17 18:35:50 +1100

    finish class 1

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,9 +2,16 @@ public class Hello {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello, PAUL1!");
         System.out.println("Hello, PAUL2!");
 
-        System.out.println("Hello, PAUL1!");
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
 
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
 }

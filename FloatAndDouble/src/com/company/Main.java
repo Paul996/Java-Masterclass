53d9149a845f73e2245c886db190f8594981d6ec
paul <pliu2468@gmail.com>
2020-02-06 15:09:47 +1100

    finish the project FloatAndDouble

--- FloatAndDouble/src/com/company/Main.java
+++ FloatAndDouble/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("double minimum value = " + myMinDoubleValue);
+        System.out.println("double maximum value = " + myMaxDoubleValue);
+
     }
 }

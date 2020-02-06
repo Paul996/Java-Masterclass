2d664cad39a10544dbb2540db557a59d03573f63
paul <pliu2468@gmail.com>
2020-02-06 15:33:04 +1100

    create an new project PoundsToKilograms

---
+++ PoundsToKilograms/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    double poundValue = 2;
+	    double kiloValue = poundValue * 0.45359237;
+        System.out.println(poundValue + " pounds = " + kiloValue + " kilograms");
+    }
+}

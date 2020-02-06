79d9f629b8f7980345a321ae6383c6ba1b27a1eb
paul <pliu2468@gmail.com>
2020-02-06 14:25:01 +1100

    finish the challenge

--- PrimitiveTypesChallenge/src/com/company/Main.java
+++ PrimitiveTypesChallenge/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte byteNum = (byte)2;
+        short shortNum = (short)4;
+        int intNum = 6;
+
+        int sum = byteNum + shortNum + intNum;
+
+        double doubleNum = 50_000 * Math.pow(sum, 10);
+        long longNum = (long)doubleNum;
+        System.out.println(byteNum);
+        System.out.println(shortNum);
+        System.out.println(intNum);
+        System.out.println(doubleNum);
+        System.out.println(longNum);
     }
 }

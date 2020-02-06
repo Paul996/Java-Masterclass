9dd610bc4d1c8e1662d3dfdffaa20f84d3d57493
paul <pliu2468@gmail.com>
2020-02-06 15:38:41 +1100

    finish the project PoundsToKilograms

--- PoundsToKilograms/src/com/company/Main.java
+++ PoundsToKilograms/src/com/company/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-	    double poundValue = 2;
-	    double kiloValue = poundValue * 0.45359237;
+	    double poundValue = 200d;
+	    double kiloValue = poundValue * 0.45359237d;
         System.out.println(poundValue + " pounds = " + kiloValue + " kilograms");
+
+        double pi = 3.1415927d;
+        double xNum = 3_000_000.4_567_789;
+        System.out.println(pi);
+        System.out.println(xNum);
     }
 }

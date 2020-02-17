d8c021b53ee48ca889945cf9e9825999bfc5b19d
paul <pliu2468@gmail.com>
2020-02-17 17:20:31 +1100

    finish the project Operators of conditional logic

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -37,5 +37,20 @@ public class Main {
 
         result -= 2;
         System.out.println(result);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+        }
+
+        int topScore = 100;
+        if (topScore >= 100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore <= 100){
+            System.out.println("Greater than second top score and less than 100");
+        }
     }
 }

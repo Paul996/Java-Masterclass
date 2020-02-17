e23de4cd4d3731243132ad6026338fcb719d3dbc
paul <pliu2468@gmail.com>
2020-02-17 16:34:36 +1100

    String test

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -8,14 +8,25 @@ public class Main {
         char myUnicodeChar = '\u0044';
         System.out.println(myChar);
         System.out.println(myUnicodeChar);
-
         char myCopyrightChar = '\u00A9';
         System.out.println(myCopyrightChar);
 
         boolean myTrueBooleanValue = true;
         boolean myFalseBooleanValue = false;
-
         boolean isCustomerOverTwentyOne = true;
+
+        //byte
+        //short
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+        String myString = "This is a string";
+        System.out.println(myString);
+        myString = myString + ", and this is more.";
+        System.out.println(myString);
 
     }
 }

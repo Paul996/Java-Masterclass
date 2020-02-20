071e952ec21f1ce453592e5da52e1dd6455cbde5
paul <pliu2468@gmail.com>
2020-02-20 15:27:06 +1100

    methods challenge

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -3,8 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        calculateScore(true, 800, 5, 100);
-        calculateScore(false, 1000, 8, 200);
+
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your high score was " + highScore);
+
+        displayHistoryScorePosition("Paul", calculateHistoryScorePosition(1500));
+        displayHistoryScorePosition("Tim", calculateHistoryScorePosition(900));
+        displayHistoryScorePosition("Jerry", calculateHistoryScorePosition(400));
+        displayHistoryScorePosition("Frank", calculateHistoryScorePosition(50));
 
     }
 
@@ -12,11 +18,26 @@ public class Main {
         if (gameOver) {
             int finalScore = score + levelCompleted * bonus;
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         } else {
             return -1;
         }
     }
 
+    public static void displayHistoryScorePosition (String name, int position) {
+        System.out.println(name + " managed to get the position " + position + " on the high table");
+        System.out.println();
+    }
+
+    public static int calculateHistoryScorePosition (int score) {
+        if (score > 1000){
+            return 1;
+        } else if (score > 500 && score < 1000) {
+            return 2;
+        } else if (score > 100 && score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
 }

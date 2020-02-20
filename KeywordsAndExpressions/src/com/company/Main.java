e6c149cc768152cdcc7de5aa786c7ff814c75fd0
paul <pliu2468@gmail.com>
2020-02-20 14:28:50 +1100

    if keyword test

--- KeywordsAndExpressions/src/com/company/Main.java
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -10,5 +10,17 @@ public class Main {
         if (highScore == 50) {
             System.out.println("This is an expression");
         }
+
+        int myVariable = 50;
+
+        System.out.println("_____________________");
+        boolean gameOver = true;
+        int score = 500;
+        int levelCompleted = 5;
+        int bonus = 1000;
+
+        if (score == 5000) {
+            System.out.println("Your score was 5000");
+        }
     }
 }
